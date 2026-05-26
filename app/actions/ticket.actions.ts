d275c087a6d7297aba3c5dd7e3a4dc7646a67b28
uwilaMinango <uwilaminango2604@yahoo.com>
2026-05-26 10:59:52 +0200

    Prisma client set up/First ticket Submitted

--- app/actions/ticket.actions.ts
+++ app/actions/ticket.actions.ts
@@ -1,10 +1,12 @@
 'use server'
 import * as Sentry from "@sentry/nextjs";
+import {prisma} from '../db/prisma';
+import {revalidatePath} from 'next/cache';
 
 export async function createTicket(prevState:{ success: boolean, message: string}, formData: FormData): Promise<{success: boolean; message: string}> {
 
     try{
-        throw new Error('Simulated Prisma error for testing');
+       
 
         const subject = formData.get('subject') as string;
         const description = formData.get('description') as string;
@@ -14,6 +16,21 @@ export async function createTicket(prevState:{ success: boolean, message: string
             Sentry.captureMessage('Validation Error: Missing ticket fields', 'warning');
             return {success: false, message: 'All fields are required'}
         }
+
+        //Create ticket
+        const ticket = await prisma.ticket.create({
+            data: {subject, description, priority}
+        })
+
+        Sentry.addBreadcrumb({
+            category: 'ticket',
+            message: `Ticket created: ${ticket.id}`,
+            
+        });
+
+        Sentry.captureMessage(`Ticket was created successfully: ${ticket.id}`);
+
+        revalidatePath('/tickets');
 
         return {success: true, message: 'ticket created successfully'}
 
