ae4dc4e19403f0f31a88393f6975e8a316fc6501
uwilaMinango <uwilaminango2604@yahoo.com>
2026-05-27 15:38:38 +0200

    modifications to navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { getCurrentUser } from '../lib/current-user';
 
-const Navbar = () => {
+const Navbar = async () => {
+
+  const user = await getCurrentUser();
+
   return(
     <nav className='bg-white border-b border-gray-200 px-6 py-4 flex justify-between items-center'>
       <div>
@@ -9,38 +13,44 @@ const Navbar = () => {
         </Link>
       </div>
       <div className='flex items-center space-x-4'>
-        
-            <Link
-              href='/tickets/new'
-              className='hover:underline text-gray-700 transition'
-            >
-              New Ticket
-            </Link>
-            <Link
+            { user ? (
+              <>
+              <Link
+                href='/tickets/new'
+                className='hover:underline text-gray-700 transition'
+                >
+                  New Ticket
+                </Link>
+              <Link
               href='/tickets'
               className='hover:underline text-gray-700 transition'
-            >
-              My Tickets
-            </Link>
-
-            <Link
+              >
+                My Tickets
+              </Link> 
+              
+              <Link
+                href='/logout'
+                className='bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition'
+              >
+                Logout
+              </Link>              
+              </>
+            ) : (
+              <>
+              <Link
               href='/login'
               className='text-blue-600 hover:underline transition'
-            >
-              Login
-            </Link>
-            <Link
-              href='/register'
-              className='bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition'
-            >
-              Register
-            </Link>         
-            <Link
-              href='/logout'
-              className='bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition'
-            >
-              Logout
-            </Link>         
+              >
+                Login
+              </Link>
+              <Link
+                href='/register'
+                className='bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition'
+              >
+                Register
+              </Link>  
+              </>
+            ) }
       </div>
     </nav>
   );
--- app/lib/current-user.ts
+++ app/lib/current-user.ts
@@ -25,6 +25,7 @@ export async function getCurrentUser() {
 
         return user;
     }catch(error){
-
+        console.log('Error getting the current user', error);  
+        return null;    
     }
 }
