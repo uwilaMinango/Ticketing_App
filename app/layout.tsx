a6c15e6977d490373e10024ff548ce05b4ace919
uwilaMinango <uwilaminango2604@yahoo.com>
2026-05-27 07:19:23 +0200

    Created Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import  Navbar   from "./components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Navbar />
+
         {children}
+        
         <Toaster position="top-center"/>
         </body>
     </html>
