212936e066aeab2160a765eed471bade58879e7e
uwilaMinango <uwilaminango2604@yahoo.com>
2026-05-27 14:37:22 +0200

    Authentication Action Added

---
+++ app/actions/auth.actions.ts
@@ -0,0 +1,85 @@
+'use server';
+
+import { prisma } from '../db/prisma';
+import bcrypt from 'bcryptjs';
+import { logEvent } from '../utils/sentry';
+import { signAuthToken, setAuthCookie } from '../lib/auth';
+
+type ResponseResult = {
+    success: boolean;
+    message: string;
+}
+
+//Register new user
+export async function registerUser(prevState: ResponseResult, formData:
+    FormData ): Promise<ResponseResult> {
+
+    try{
+        const name = formData.get( 'name' ) as string;
+        const email = formData.get( 'email' ) as string;
+        const password = formData.get( 'password' ) as string;
+
+        if(!name || !email || !password){
+            logEvent(
+                'Validation error: Missing register fields',
+                'auth',
+                {name, email},
+                'warning'
+            );
+
+            return {success: false, message: 'All fields are required.'};
+        }
+
+        //Check if user esists
+        const existingUser = await prisma.user.findUnique({
+            where: {email}
+        });
+
+        if(existingUser){
+            logEvent(
+                `Registration failed: User already exists - ${email}`,
+                'auth',
+                {email},
+                'warning'
+            )
+
+            return {success: false, message: 'User already exists.'};
+        }  
+        
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //Create the User
+        const user = await prisma.user.create({
+            data: {
+                name, 
+                email,
+                password: hashedPassword
+            }
+        })
+
+        //Sign and set the auth token
+        const token = await signAuthToken({userId: user.id});
+        await setAuthCookie(token);
+
+        logEvent(
+            `User registered successfully: ${email}`,
+            'auth',
+            {userId: user.id, email},
+            'info'
+        )
+
+        return {success: true, message: 'Registration Successful'};
+    }catch(error) {
+        logEvent(
+            'Unexpected error during registration',
+            'auth',
+            {},
+            'error',
+            error
+        )
+        return {success: false, message: 'Something went wrong, please try again.'}
+    }    
+
+    
+}
