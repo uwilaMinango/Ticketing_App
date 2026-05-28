4dae0f81edb6e3950f8cdd6ff74aea516a381e28
uwilaMinango <uwilaminango2604@yahoo.com>
2026-05-28 08:45:26 +0200

    Close ticket action created

--- app/actions/ticket.actions.ts
+++ app/actions/ticket.actions.ts
@@ -143,3 +143,54 @@ export async function getTicketById(id: string){
         return null;
     }
 }
+
+//Close Ticket
+export async function closeTicket(prevState:{ success: boolean, message: string }, formData: FormData): Promise<{ success: boolean; message: string}>{
+    const ticketId = Number(formData.get('ticketId'));
+
+    if(!ticketId){
+        logEvent(
+            'Missing Ticket ID',
+            'Ticket',
+            {},
+            'warning'
+        );
+        return { success:false, message: 'Ticket Id is required'}
+    }
+
+    const user = await getCurrentUser();
+
+    if(!user){
+         logEvent(
+            'Missing User ID',
+            'Ticket',
+            {},
+            'warning'
+        );
+
+        return{ success: false, message: 'unauthorized' }
+    }
+
+    const ticket = await prisma.ticket.findUnique({
+        where: {id: ticketId}
+    });
+
+    if(!ticket || ticket.userId !== user.id){
+        logEvent(
+            'Unauthorized ticket close attempt',
+            'ticket',
+            { ticketId, userId: user.id }, 
+            'warning'
+        );
+
+        return { success: false, message: 'You are not authorized to close this ticket' }
+    }
+
+    await prisma.ticket.update({
+        where: { id: ticketId }, 
+        data: { status: 'Closed' }
+    });
+
+    revalidatePath('/tickets');
+    revalidatePath(`/tickets/${ticketId}`);
+}
