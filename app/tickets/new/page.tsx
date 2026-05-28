4ebc7cb09ba8164840885dc81bf0879a486d1f17
uwilaMinango <uwilaminango2604@yahoo.com>
2026-05-28 09:16:50 +0200

    Final commit

--- app/tickets/new/page.tsx
+++ app/tickets/new/page.tsx
@@ -1,6 +1,14 @@
 import NewTicketForm from './ticket-form';
+import { getCurrentUser } from '../../lib/current-user';
+import { redirect } from 'next/navigation'
 
-const NewTicketPage = () => {
+const NewTicketPage = async () => {
+  const user = await getCurrentUser();
+
+  if(!user){
+    redirect('/login');
+  }
+
   return (
     <div className="min-h-screen bg-blue-50 flex items-center justify-center px-4">
         <NewTicketForm />
